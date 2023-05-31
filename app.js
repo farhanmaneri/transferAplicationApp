ea229c8c4490f9294be2f8e50bbf19bfe8f47482
farhanmaneri <farhanmaneri@gmail.com>
2023-05-31 12:34:53 -0700

    changes made

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ const print = () => {
 event.preventDefault()
   
     var gender = document.getElementById('gender').value;
-    var name = document.getElementById('name').value;
+    var name = document.getElementById('name').value.toUpperCase();
     var designation = document.getElementById('designation').value;
-    var current_station = document.getElementById('current_station').value;
-    var to_station = document.getElementById('to_station').value;
+    var current_station = document.getElementById('current_station').value.toUpperCase();
+    var to_station = document.getElementById('to_station').value.toUpperCase();
     var remarks = document.getElementById('remarks').value;
+    var old_station = document.getElementById('old_station').value;
+    var new_station = document.getElementById('new_station').value;
    
 
     var table = document.getElementById('dataTable').getElementsByTagName('tbody')[0];
@@ -27,13 +29,13 @@ event.preventDefault()
     var serialCell = newRow.insertCell(0)
     var nameCell = newRow.insertCell(1);
     var current_stationCell = newRow.insertCell(2);
-    var to_stationCell = newRow.insertCell(2);
+    var to_stationCell = newRow.insertCell(3);
     var remarksCell = newRow.insertCell(4);
 
     serialCell.innerHTML = rowIndex
     nameCell.innerHTML = gender +" "+name  +","+" "+ designation
-    current_stationCell.innerHTML = current_station;
-    to_stationCell.innerHTML = to_station;
+    current_stationCell.innerHTML = old_station +" "+current_station;
+    to_stationCell.innerHTML = new_station +" "+to_station;
     remarksCell.innerHTML = remarks;
 
     document.getElementById('myForm').reset();
