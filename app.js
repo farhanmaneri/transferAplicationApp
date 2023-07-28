1293471eef2cc22eaabb365c1dfc66d2850ca0a4
farhanmaneri <farhanmaneri@gmail.com>
2023-07-28 00:26:13 -0700

    cnic and emis code options add

--- app.js
+++ app.js
@@ -10,28 +10,38 @@ const print = () => {
 event.preventDefault()
   
     var gender = document.getElementById('gender').value;
+    // name 
     var name = document.getElementById("name").value;
     var words = name.split(" ");
     var capitalized = words.map(function (word) {
       return word.charAt(0).toUpperCase() + word.slice(1);
     });
     var capitalizedName = capitalized.join(" ");
-  
+  // cnic 
+    var cnic = document.getElementById('cnic').value
+// designation 
     var designation = document.getElementById('designation').value;
+
+    // currentSchool name 
     var current_station = document.getElementById('current_station').value;
     var words = current_station.split(" ");
     var capitalized = words.map(function (word) {
       return word.charAt(0).toUpperCase() + word.slice(1);
     });
     var currentSchool = capitalized.join(" ");
-
+    // currentSchool Emis Code 
+    var currentEmis = document.getElementById('currentEmis').value;
+    // New School/ station name 
     var to_station = document.getElementById('to_station').value;
     var words = to_station.split(" ");
     var capitalized = words.map(function (word) {
       return word.charAt(0).toUpperCase() + word.slice(1);
     });
     var newSchool = capitalized.join(" ");
-
+    
+    // New School Emis Code 
+    var newSchoolEmis = document.getElementById('newStationEmis').value;
+    // remarks 
     var remarks = document.getElementById('remarks').value;
     var words = remarks.split(" ");
     var capitalized = words.map(function (word) {
@@ -58,9 +68,9 @@ event.preventDefault()
     var remarksCell = newRow.insertCell(4);
 
     serialCell.innerHTML = rowIndex
-    nameCell.innerHTML = gender +" "+capitalizedName  +","+" "+ designation
-    current_stationCell.innerHTML = old_station +" "+currentSchool;
-    to_stationCell.innerHTML = new_station +" "+newSchool;
+    nameCell.innerHTML = gender +" "+ capitalizedName  +","+" "+ designation +"<br>"+"("+ cnic +")";
+    current_stationCell.innerHTML = old_station +" "+currentSchool +"<br>" + "("+ currentEmis +")";
+    to_stationCell.innerHTML = new_station +" "+newSchool+"<br>" + "("+ newSchoolEmis +")";
     remarksCell.innerHTML = capitalizedRemarks;
 
     document.getElementById('myForm').reset();
